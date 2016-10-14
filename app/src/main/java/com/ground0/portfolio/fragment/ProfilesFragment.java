87c9941030ccfe8edb95fc73a88680614d08ecc7
00-00-00 <nexusa8@gmail.com>
2016-10-15 00:00:39 +0530

    1. Item listener for links

--- app/src/main/java/com/ground0/portfolio/fragment/ProfilesFragment.java
+++ app/src/main/java/com/ground0/portfolio/fragment/ProfilesFragment.java
@@ -1,11 +1,17 @@
 package com.ground0.portfolio.fragment;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import com.ground0.portfolio.R;
 import com.ground0.portfolio.activity.HomeActivity;
 import com.ground0.portfolio.core.components.BaseFragment;
@@ -32,5 +38,24 @@ public class ProfilesFragment extends BaseFragment<HomeActivity> {
     mRootView = inflater.inflate(R.layout.fragment_profiles, container, false);
     ButterKnife.bind(this, mRootView);
     return mRootView;
+  }
+
+  @OnClick(R.id.f_profiles_github) public void launchGithub() {
+    Uri uri = Uri.parse("http://www.github.com/00-00-00");
+    startBrowser(uri);
+  }
+
+  @OnClick(R.id.f_profiles_linked) public void launchIn() {
+    Uri uri = Uri.parse("http://www.linkedin.com/in/00-00-00");
+    startBrowser(uri);
+  }
+
+  private void startBrowser(Uri uri) {
+    try {
+      Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uri);
+      getActivity().startActivity(launchBrowser);
+    } catch (ActivityNotFoundException e) {
+      Snackbar.make(mRootView, "Browser missing", Snackbar.LENGTH_SHORT).show();
+    }
   }
 }
